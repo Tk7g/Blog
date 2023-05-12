98985b2f458cb4a8f6ede19ae7fe146aa7d7967a
tk7g <atulgaa360@gmail.com>
2023-05-12 12:45:47 +0800

    react bootstrap холболоо

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import  Alert  from 'react-bootstrap/Alert'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      My blog
-    </>
+    <div className={styles.container}>
+    <main className={styles.main}>
+      <div>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+      </div>
+    </main>
+    </div>
   )
 }
